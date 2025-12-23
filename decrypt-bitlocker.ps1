71dfcfdf281eed74ec8af906c02174062bea61f1
lea13245 <152735283+lea13245@users.noreply.github.com>
2025-12-23 18:56:58 -0300

    Update decrypt-bitlocker.ps1

--- decrypt-bitlocker.ps1
+++ decrypt-bitlocker.ps1
@@ -1,7 +1,5 @@
 #region UI / BANNER
 cls
-cls
-
 Write-Host @"
   _________                                         .__                           
  /   _____/ ___________   ____   ____   ____   _____|  |__ _____ _______   ____   
@@ -9,16 +7,7 @@ Write-Host @"
  /        \  \___|  | \/\  ___/\  ___/|   |  \\___ \|   Y  \/ __ \|  | \/\  ___/  
 /_______  /\___  >__|    \___  >\___  >___|  /____  >___|  (____  /__|    \___  > 
         \/     \/            \/     \/     \/     \/     \/     \/            \/  
-   _____  .__  .__  .__                            
-  /  _  \ |  | |  | |__|____    ____   ____  ____  
- /  /_\  \|  | |  | |  \__  \  /    \_/ ___\/ __ \ 
-/    |    \  |_|  |_|  |/ __ \|   |  \  \__\  ___/ 
-\____|__  /____/____/__(____  /___|  /\___  >___  >
-        \/                  \/     \/     \/    \/ 
 "@ -ForegroundColor White
-
-Start-Sleep 1
-cls
 #endregion
 
 #region CORE
@@ -27,74 +16,60 @@ $Errors  = @()
 
 function Status($m){ Write-Host "[*] $m" -ForegroundColor Cyan }
 function Err($m){ Write-Host "[X] $m" -ForegroundColor Red }
-
-function SafeExec($Name,[scriptblock]$b){
-    try {
-        Status $Name
-        & $b
-    } catch {
-        $Errors += [pscustomobject]@{
-            Step  = $Name
-            Error = $_.Exception.Message
-        }
-        Err "$Name failed"
-    }
-}
 #endregion
 
-#region PARTITION ENUMERATION (MAGNET-LIKE)
-Status "Enumerating disks and partitions (safe mode)"
-
-$volumes = @()
+#region ENUMERATION (MAGNET-LIKE, SAFE)
+Status "Enumerating disks, partitions and volumes"
 
 try {
-    $volumes = Get-WmiObject Win32_LogicalDisk -ErrorAction Stop
+    $volumes = Get-CimInstance Win32_LogicalDisk -ErrorAction Stop
 } catch {
-    Err "WMI Win32_LogicalDisk failed"
-    $Errors += [pscustomobject]@{
-        Step  = "Get-WmiObject Win32_LogicalDisk"
-        Error = $_.Exception.Message
-    }
+    Err "Failed to enumerate logical disks"
+    $Errors += $_.Exception.Message
+    $volumes = @()
 }
 
 foreach ($v in $volumes) {
 
-    $size = "N/A"
-    $free = "N/A"
-
-    if ($v.Size -and $v.Size -gt 0) {
-        $size = [math]::Round($v.Size / 1GB,2)
-    }
-
-    if ($v.FreeSpace -and $v.FreeSpace -gt 0) {
-        $free = [math]::Round($v.FreeSpace / 1GB,2)
-    }
+    $size = if ($v.Size) { [math]::Round($v.Size / 1GB,2) } else { "N/A" }
+    $free = if ($v.FreeSpace) { [math]::Round($v.FreeSpace / 1GB,2) } else { "N/A" }
 
     $Results += [pscustomobject]@{
         Drive      = $v.DeviceID
         Label      = $v.VolumeName
         FileSystem = $v.FileSystem
         Type       = switch ($v.DriveType) {
-            2 { "Removable (USB)" }
-            3 { "Fixed Disk" }
+            2 { "Removable" }
+            3 { "Fixed" }
+            4 { "Network" }
             5 { "Optical" }
             default { "Other" }
         }
         SizeGB     = $size
         FreeGB     = $free
         Encryption = "Not evaluated"
-        Cipher     = "N/A"
     }
 }
-
 #endregion
 
-#region OUTPUT
+#region OUTPUT (SAFE FOR CMD / IEX)
 if ($Results.Count -gt 0) {
-    $Results | Out-GridView -Title "EDD+ Partition & Encryption Overview"
+    if (Get-Command Out-GridView -ErrorAction SilentlyContinue) {
+        $Results | Out-GridView -Title "EDD+ Volume Overview"
+    } else {
+        $Results | Format-Table -AutoSize
+    }
+} else {
+    Err "No volumes detected"
 }
 
 if ($Errors.Count -gt 0) {
-    $Errors | Out-GridView -Title "EDD+ Errors"
+    Write-Host ""
+    Err "Errors:"
+    $Errors | Format-List
 }
+
+Write-Host ""
+Write-Host "Execution finished. Press ENTER to exit." -ForegroundColor DarkGray
+[void][System.Console]::ReadLine()
 #endregion
